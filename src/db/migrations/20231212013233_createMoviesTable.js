31a3efcfd60c5919b1b414fe29b682cc966e97c1
Lorraine <dmjlorraine@gmail.com>
2023-12-12 13:10:59 -0600

    fixed movies migration

--- src/db/migrations/20231212013233_createMoviesTable.js
+++ src/db/migrations/20231212013233_createMoviesTable.js
@@ -1,11 +1,13 @@
 exports.up = function (knex) {
-  table.increments("movie_id").primary();
-  table.string("title");
-  table.integer("runtime_in_minutes");
-  table.string("rating");
-  table.text("description");
-  table.string("image_url");
-  table.timestamps(true, true);
+  knex.schema.createTable("movies", (table) => {
+    table.increments("movie_id").primary();
+    table.string("title");
+    table.integer("runtime_in_minutes");
+    table.string("rating");
+    table.text("description");
+    table.string("image_url");
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function (knex) {
