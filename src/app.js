a236ae65f1e75332b8eb391040bca9161d5b0602
Lorraine <dmjlorraine@gmail.com>
2023-12-14 14:32:19 -0600

    finished get /movies route, bug fixes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-if (process.env.USER) require("dotenv").config();
+if (process.env.DATABASE_USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
+const notFoundHandler = require("./errors/notFoundHandler");
+const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
-app.use("/reviews", reviewsRouter);
-app.use("/theaters", theatersRouter);
+// app.use("/reviews", reviewsRouter);
+// app.use("/theaters", theatersRouter);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 module.exports = app;
--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,3 +1,23 @@
-const knex = require("../db/connection");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const service = require("./movies.service");
 
-module.exports = {};
+//controller functions
+async function list(req, res, next) {
+  // console.log("QUERY:", req.query);
+  const { is_showing } = req.query;
+  // console.log("IS SHOWING OR NOT:", is_showing);
+  // console.log(typeof is_showing);
+  if (is_showing === "true") {
+    const isShowingMovies = await service.listIsShowing();
+    // console.log("IS SHOWING MOVIES:", isShowingMovies);
+    res.json({ data: isShowingMovies });
+  } else {
+    const allMovies = await service.list();
+    // console.log("ALL MOVIES:", allMovies);
+    res.json({ data: allMovies });
+  }
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+};
