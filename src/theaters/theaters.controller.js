5d876afb55d99787a5e45800616154b60169958c
Lorraine <dmjlorraine@gmail.com>
2023-12-15 03:41:59 -0600

    get /theaters route fixed

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -8,23 +8,24 @@ async function list(req, res, next) {
   // console.log(allTheatersAndMovies);
 
   const reduceMoviesAndTheaters = reduceProperties("name", {
-    // theater_id: ["theater", "theater_id"],
+    //specify the information needed in each object
     name: ["name"],
     address_line_1: ["address_line_1"],
     address_line_2: ["address_line_2"],
     city: ["city"],
     state: ["state"],
     zip: ["zip"],
-    // movie_id: ["movies", null, "movie_id"],
+    //add new array to each object called "movies"
     title: ["movies", null, "title"],
     runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
     rating: ["movies", null, "rating"],
   });
 
+  //call on function with the obtained data
   const theatersWithMovies = reduceMoviesAndTheaters(allTheatersAndMovies);
-  console.log(theatersWithMovies);
+  // console.log(theatersWithMovies);
 
-  res.json({ data: theatersWithMovies[0] });
+  res.json({ data: theatersWithMovies });
 }
 
 module.exports = {
