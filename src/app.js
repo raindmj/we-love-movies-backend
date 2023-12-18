8850796f645cafdfc8259f38c513352db47cb22d
Lorraine <dmjlorraine@gmail.com>
2023-12-18 17:05:17 -0600

    added cors to app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,19 @@ const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 const notFoundHandler = require("./errors/notFoundHandler");
 const errorHandler = require("./errors/errorHandler");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
 
+//Not found handler
 app.use(notFoundHandler);
 
+//Error handler
 app.use(errorHandler);
 
 module.exports = app;
