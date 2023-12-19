bfaa26f4453eec1d031a3636870f2fc8c55a90c8
Lorraine <dmjlorraine@gmail.com>
2023-12-19 16:59:05 -0600

    fixed /theaters route

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -5,25 +5,25 @@ const service = require("./theaters.service");
 async function list(req, res, next) {
   const allTheatersAndMovies = await service.listTheaters();
 
-  // console.log(allTheatersAndMovies);
-
-  const reduceMoviesAndTheaters = reduceProperties("name", {
+  //reduce the data into only the information needed
+  const reduceMoviesAndTheaters = reduceProperties("theater_id", {
     //specify the information needed in each object
-    name: ["name"],
-    address_line_1: ["address_line_1"],
-    address_line_2: ["address_line_2"],
-    city: ["city"],
-    state: ["state"],
-    zip: ["zip"],
+    theaters_created_at: ["created_at"],
+    theaters_updated_at: ["updated_at"],
     //add new array to each object called "movies"
+    movie_id: ["movies", null, "movie_id"],
     title: ["movies", null, "title"],
     runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
     rating: ["movies", null, "rating"],
+    description: ["movies", null, "description"],
+    image_url: ["movies", null, "image_url"],
+    movies_created_at: ["movies", null, "created_at"],
+    movies_updated_at: ["movies", null, "updated_at"],
+    is_showing: ["movies", null, "is_showing"],
   });
 
   //call on function with the obtained data
   const theatersWithMovies = reduceMoviesAndTheaters(allTheatersAndMovies);
-  // console.log(theatersWithMovies);
 
   res.json({ data: theatersWithMovies });
 }
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,28 +1,19 @@
 const knex = require("../db/connection");
 
-function listTheaters(theaterId) {
+function listTheaters() {
   return knex("theaters as t")
     .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
     .join("movies as m", "m.movie_id", "mt.movie_id")
     .select(
-      // "t.theater_id",
-      "t.name",
-      "t.address_line_1",
-      "t.address_line_2",
-      "t.city",
-      "t.state",
-      "t.zip",
-      "m.title",
-      "m.runtime_in_minutes",
-      "m.rating"
+      "m.*",
+      "t.*",
+      "mt.*",
+      "m.created_at as movies_created_at",
+      "m.updated_at as movies_updated_at",
+      "t.created_at as theaters_created_at",
+      "t.updated_at as theaters_updated_at"
     );
-  // .where({ "t.theater_id": theaterId });
-  // .groupBy("t.theater_id");
 }
-
-/* function listMovies() {
-  return knex("movies as m").select("*");
-} */
 
 module.exports = {
   listTheaters,
