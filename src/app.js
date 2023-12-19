1c4b17a9068a94df10da221cc1ac5e544b7cd5d8
Lorraine <dmjlorraine@gmail.com>
2023-12-18 18:40:58 -0600

    bug fixing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-if (process.env.DATABASE_USER) require("dotenv").config();
+if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
